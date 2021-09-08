3b7d83dab8f1913f4d0347078da8dfaf9cbd5d3b
stevenacz <scoailazaa@gmail.com>
2021-09-08 16:55:32 -0500

    ⭐️ improved

--- src/components/layout/Styles.ts
+++ src/components/layout/Styles.ts
@@ -1,16 +1,16 @@
 import styled from 'styled-components';
 
-export const LayoutStyled = styled.section`
+export const LayoutStyled = styled.main`
 	border: 1px solid black;
 	min-height: 100vh;
 	max-width: 1200px;
 	margin: 0 auto;
 
-	@media (max-width: 991px) {
+	/* @media (max-width: 1200px) {
 		background-color: red;
 	}
 
 	@media (max-width: 375px) {
 		background-color: blue;
-	}
+	} */
 `;
