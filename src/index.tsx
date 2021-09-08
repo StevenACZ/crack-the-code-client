6c50cc005137df4c8120b4d969728c7d8bba3af8
stevenacz <scoailazaa@gmail.com>
2021-09-08 16:19:45 -0500

    🧰 react-redux and redux added to the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 // React
-import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './redux/store/store';
 
 // Components
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
