90e306f868a8a90c5814655ecabfd6a4dcbd9954
stevenacz <scoailazaa@gmail.com>
2021-09-08 17:25:44 -0500

    ⭐️ improved

--- src/pages/Styles.ts
+++ src/pages/Styles.ts
@@ -1,18 +1,9 @@
 import styled from 'styled-components';
 
 export const HomeScreenStyled = styled.section`
-	grid-area: header;
-	height: 100px;
-	padding: 30px 0px;
-	display: flex;
-	justify-content: space-between;
-	align-content: center;
-
 	@media (max-width: 1200px) {
-		padding: 30px 40px;
 	}
 
 	@media (max-width: 375px) {
-		padding: 30px 20px;
 	}
 `;
