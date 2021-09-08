7f1f56529a6daabd6f9b9069f363e0361635278f
stevenacz <scoailazaa@gmail.com>
2021-09-08 17:25:29 -0500

    🧰 Banner added

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -4,10 +4,21 @@ import React from 'react';
 // Styles
 import { HomeScreenStyled } from './Styles';
 
+// Components
+import Banner from '../components/banner/Banner';
+
 interface HomeProps {}
 
 const HomeScreen: React.FC<HomeProps> = () => {
-	return <HomeScreenStyled>Home</HomeScreenStyled>;
+	return (
+		<HomeScreenStyled>
+			<Banner
+				title="Cursos de programación online para todo el mundo"
+				subtitle="Clases prácticas, concretas y enfocadas en las habilidades de mayor
+					demanda laboral."
+			/>
+		</HomeScreenStyled>
+	);
 };
 
 export default HomeScreen;
