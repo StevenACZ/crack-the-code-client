6beeaccf030ff6b6d9f2c05822851c7b630f71ac
stevenacz <scoailazaa@gmail.com>
2021-09-09 10:50:23 -0500

    🧰 Footer to it added

--- src/components/layout/footer/Styles.ts
+++ src/components/layout/footer/Styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const FooterStyled = styled.section`
+export const FooterStyled = styled.footer`
 	width: 100%;
 	height: 100px;
 	border-radius: 10px;
