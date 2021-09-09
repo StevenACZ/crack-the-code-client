77289c80840b09f82add252eecc4a2bd2084377b
stevenacz <scoailazaa@gmail.com>
2021-09-08 20:10:05 -0500

    ⭐️ improved

--- src/components/course-list/Styles.ts
+++ src/components/course-list/Styles.ts
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 
 export const CourseListStyled = styled.ul`
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(270px, 1fr));
+	grid-column-gap: 24px;
+	grid-row-gap: 40px;
+
 	@media (max-width: 1200px) {
 	}
 
