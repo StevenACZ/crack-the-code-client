bf2b19d88053f8e07763ca1e806d86e60852b14c
stevenacz <scoailazaa@gmail.com>
2021-09-08 20:09:45 -0500

    ⭐️ course list item improved

--- src/components/course-list/course-list-item/CourseListItem.tsx
+++ src/components/course-list/course-list-item/CourseListItem.tsx
@@ -5,11 +5,21 @@ import React from 'react';
 import moment from 'moment';
 import 'moment/locale/es';
 
-// Styles
-import { CourseListItemStyled } from './Styles';
-
 // Interfaces
 import { ReqResCourse } from '../../../interfaces/reqResCourse.interfaces';
+
+// Styles
+import {
+	CourseListItemStyled,
+	Header,
+	Body,
+	BodyTop,
+	BodyBottom,
+	Free,
+} from './Styles';
+
+// Components
+import CoursePrice from './course-price/CoursePrice';
 
 const CourseListItem: React.FC<ReqResCourse> = ({
 	name,
@@ -20,59 +30,89 @@ const CourseListItem: React.FC<ReqResCourse> = ({
 	discount_percentage,
 	amount_after_discount_usd,
 	amount_after_discount_pen,
-	thumbnail,
+	// thumbnail,
 	next_start_date,
 	duration,
 }) => {
 	return (
 		<CourseListItemStyled>
-			<h2>{name}</h2>
+			<Header>{/* {thumbnail && <img src={thumbnail!} alt="asd" />} */}</Header>
 
-			{/* isFree? */}
-			{parseInt(amount_pen) === 0 && parseInt(amount_usd) === 0 ? (
-				<p>GRATIS</p>
-			) : (
-				<>
-					{!(parseInt(amount_usd) === 0) && <p>Precio ${amount_usd}</p>}
-					{!(parseInt(amount_pen) === 0) && <p>Precio S/{amount_pen}</p>}
-				</>
-			)}
+			<Body>
+				<BodyTop>
+					<h2>{name}</h2>
 
-			{/* hasDiscount? */}
-			{has_discount && (
-				<div>
-					<br />
-					<p>DESCUENTO DEL {parseInt(discount_percentage)}%</p>
-					{!(parseInt(amount_after_discount_usd) === 0) && (
-						<p>Nuevo precio ${amount_after_discount_usd}</p>
+					<p>
+						Edad:{' '}
+						{age_range
+							.replaceAll('_', ' ')
+							.replace('to', 'a')
+							.replace(' plus', '+')}{' '}
+						años
+					</p>
+				</BodyTop>
+
+				<BodyBottom>
+					{/* isFree? */}
+					{parseInt(amount_pen) === 0 && parseInt(amount_usd) === 0 ? (
+						<Free>GRATIS</Free>
+					) : (
+						<>
+							{!has_discount && (
+								<div>
+									{!(parseInt(amount_usd) === 0) && (
+										<CoursePrice
+											price={parseInt(amount_usd)}
+											badge="usd"
+											size="1"
+										/>
+									)}
+									{!(parseInt(amount_pen) === 0) && (
+										<CoursePrice
+											price={parseInt(amount_pen)}
+											badge="pen"
+											size="1"
+										/>
+									)}
+								</div>
+							)}
+						</>
 					)}
-					{!(parseInt(amount_after_discount_pen) === 0) && (
-						<p>Nuevo precio S/{amount_after_discount_pen}</p>
+
+					{/* hasDiscount? */}
+					{has_discount && (
+						<div>
+							<p>DESCUENTO DEL {parseInt(discount_percentage)}%</p>
+							{!(parseInt(amount_after_discount_usd) === 0) && (
+								<CoursePrice
+									price={parseInt(amount_after_discount_usd)}
+									discount_price={parseInt(amount_usd)}
+									badge="usd"
+									size="2"
+								/>
+							)}
+							{!(parseInt(amount_after_discount_pen) === 0) && (
+								<CoursePrice
+									price={parseInt(amount_after_discount_pen)}
+									discount_price={parseInt(amount_pen)}
+									badge="pen"
+									size="2"
+								/>
+							)}
+						</div>
 					)}
-				</div>
-			)}
 
-			{/* nextStartDate? */}
-			{next_start_date && (
-				<p>
-					Fecha de inicio:{' '}
-					{moment(next_start_date).locale('es').format(`D [de] MMMM`)}
-				</p>
-			)}
+					{/* nextStartDate? */}
+					{next_start_date && (
+						<p>
+							Fecha de inicio:{' '}
+							{moment(next_start_date).locale('es').format(`D [de] MMMM`)}
+						</p>
+					)}
 
-			{/* {thumbnail && <img src={thumbnail!} alt="asd" />} */}
-			<p>Duración: {parseInt(duration)} meses</p>
-			<p>
-				Edad:{' '}
-				{age_range
-					.replaceAll('_', ' ')
-					.replace('to', 'a')
-					.replace(' plus', '+')}{' '}
-				años
-			</p>
-			<br />
-			<hr />
-			<br />
+					<p>Duración: {parseInt(duration)} meses</p>
+				</BodyBottom>
+			</Body>
 		</CourseListItemStyled>
 	);
 };
